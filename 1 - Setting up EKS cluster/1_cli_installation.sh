5c2bd4b6d751b28ec5930c091ba911b3923f1e36
Shabeen <shabeensiyam@gmail.com>
2022-03-09 18:19:44 +0530

    Added provision eks script

--- 1 - Setting up EKS cluster/1_cli_installation.sh
+++ 1 - Setting up EKS cluster/1_cli_installation.sh
@@ -1,3 +1,11 @@
+# Install pre-requisites
+sudo yum -y install jq gettext bash-completion moreutils
+
+echo 'yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}' | tee -a ~/.bashrc && source ~/.bashrc
+
+
 # Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
@@ -9,8 +17,17 @@ chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 kubectl version --short --client
+kubectl completion bash >>  ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
 
 # Install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
+
+# Enable eksctl bash-completion
+eksctl completion bash >> ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
---
+++ 1 - Setting up EKS cluster/2_provision_eks_cluster.sh
@@ -0,0 +1,28 @@
+# Create manifest for EKS Cluster
+cat << EOF > ekscluster.yaml
+---
+apiVersion: eksctl.io/v1alpha5
+kind: ClusterConfig
+
+metadata:
+  name: ekscluster-eksctl
+  region: us-east-1
+  version: "1.19"
+
+availabilityZones: ["us-east-1a", "us-east-1b"]
+
+managedNodeGroups:
+- name: nodegroup
+  desiredCapacity: 3
+  instanceType: t3.small
+  ssh:
+    enableSsm: true
+
+ To enable all of the control plane logs, uncomment below:
+ cloudWatch:
+  clusterLogging:
+    enableTypes: ["*"]
+EOF
+
+# Create cluster from the manifest file
+eksctl create cluster -f ekscluster.yaml
