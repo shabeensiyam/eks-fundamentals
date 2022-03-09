637f11022f9c919c6c4477d3ce6df252f18f2f41
Shabeen <shabeensiyam@gmail.com>
2022-03-09 19:50:45 +0530

    Updated deploy_app.sh

--- 2 - Deploy App/1_deploy_apps.sh
+++ 2 - Deploy App/1_deploy_apps.sh
@@ -16,6 +16,11 @@ kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 kubectl get deployment eksdemo-frontend
 
+# Check Frontend App
+kubectl get service eksdemo-frontend -o wide
+ELB=$(kubectl get service ecsdemo-frontend -o json | jq -r '.status.loadBalancer.ingress[].hostname')
+curl -m3 -v $ELB
+
 # Scale Deployments
 kubectl scale deployment eksdemo-nodejs --replicas=3
 kubectl scale deployment eksdemo-crystal --replicas=3
