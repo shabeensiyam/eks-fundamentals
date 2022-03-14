cded17449a839746189a180793cd50e1a90544a9
Shabeen <shabeensiyam@gmail.com>
2022-03-14 17:07:30 +0530

    Added Helm installation commands

--- 5 - Prometheus and Grafana/1_prereq.sh
+++ 5 - Prometheus and Grafana/1_prereq.sh
@@ -1,3 +1,13 @@
+# Install Helm
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+# Bash completion for Helm
+helm completion bash >> ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+source <(helm completion bash)
+
+
 # add prometheus Helm repo
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
